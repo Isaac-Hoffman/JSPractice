963a45d20a68c4457a4212be02415b0eff9fc658
HOFFMAN <19hoffmani@gmail.com>
2018-08-29 08:51:17 -0500

    more js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+//Comment
+/*
+Multi
+Line
+*/
+a = 3.14;
+const b = 'lol';
 console.log("Hello Javascript");
+
+//value or data types
+console.log("Hello form Javascript!");
+console.log("Let's do some math...");
+console.log(4 + 7);
+console.log(12 / 0);
+console.log("Goodbye");
+console.log(a);
+console.log(b);
