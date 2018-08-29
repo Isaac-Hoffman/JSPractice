bf5d5962670a9adbab7c1cf68cd614fbe79a969e
HOFFMAN <19hoffmani@gmail.com>
2018-08-29 09:20:08 -0500

    more changes

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ Multi
 Line
 */
 a = 3.14;
-const b = 'lol';
+const b = "lol";
+let c = 0;
+c = 0;
+c += 1;
+c++;
 console.log("Hello Javascript");
 
 //value or data types
+{
 console.log("Hello form Javascript!");
 console.log("Let's do some math...");
 console.log(4 + 7);
@@ -15,3 +20,16 @@ console.log(12 / 0);
 console.log("Goodbye");
 console.log(a);
 console.log(b);
+console.log(c);
+//const d = 9;error
+}
+//console.log(d);error
+const country = "France";
+console.log(`I live in ${country}`) //show "I live in France"
+const x = 3;
+const y = 7;
+console.log(`${x} + ${y} = ${x+ y}`); //show "3 + 7 = 10"
+console.log(4+5);
+console.log("4 + 5");
+console.log("4" + "5");
+console.log(4 + "5");
